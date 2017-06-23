4b95197ac34afdeca862b6bdf08fedf81a91b9da
Matt Hale <mlhale@unomaha.edu>
2017-06-23 11:25:21 -0500

    added ember-heatmap-demo for https://github.com/MLHale/ember-heatmap

---
+++ ember-heatmap-demo/assets/dummy.js
@@ -0,0 +1,242 @@
+"use strict";
+
+
+
+define('dummy/app', ['exports', 'ember', 'dummy/resolver', 'ember-load-initializers', 'dummy/config/environment'], function (exports, _ember, _resolver, _emberLoadInitializers, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+
+  var App = void 0;
+
+  _ember.default.MODEL_FACTORY_INJECTIONS = true;
+
+  App = _ember.default.Application.extend({
+    modulePrefix: _environment.default.modulePrefix,
+    podModulePrefix: _environment.default.podModulePrefix,
+    Resolver: _resolver.default
+  });
+
+  (0, _emberLoadInitializers.default)(App, _environment.default.modulePrefix);
+
+  exports.default = App;
+});
+define('dummy/components/ember-heatmap', ['exports', 'ember-heatmap/components/ember-heatmap'], function (exports, _emberHeatmap) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function () {
+      return _emberHeatmap.default;
+    }
+  });
+});
+define('dummy/components/welcome-page', ['exports', 'ember-welcome-page/components/welcome-page'], function (exports, _welcomePage) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function () {
+      return _welcomePage.default;
+    }
+  });
+});
+define('dummy/controllers/application', ['exports', 'ember'], function (exports, _ember) {
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.default = _ember.default.Controller.extend({
+		heatmapdata: [],
+		heatmapmax: 0,
+		heatmapconfig: {
+			radius: 40,
+			maxOpacity: .9,
+			minOpacity: 0,
+			blur: .75
+		},
+		init: function init() {
+			this._super();
+			this.get('loadHeatmapData')(this);
+		},
+		loadHeatmapData: function loadHeatmapData(controller) {
+			//create some random data to test the heatmap
+			var points = [];
+			var max = 0;
+			var width = _ember.default.$(window).width();
+			var height = _ember.default.$(window).height();
+			var len = 400;
+
+			while (len--) {
+				var val = Math.floor(Math.random() * 100);
+				max = Math.max(max, val);
+				var point = {
+					x: Math.floor(Math.random() * width),
+					y: Math.floor(Math.random() * height),
+					value: val
+				};
+				points.push(point);
+			}
+			controller.set('heatmapdata', points);
+			controller.set('heatmapmax', max);
+		}
+	});
+});
+define('dummy/ember-heatmap/tests/addon.lint-test', [], function () {
+  'use strict';
+
+  QUnit.module('ESLint | addon');
+
+  QUnit.test('ember-heatmap/components/ember-heatmap.js', function (assert) {
+    assert.expect(1);
+    assert.ok(false, 'ember-heatmap/components/ember-heatmap.js should pass ESLint\n\n29:15 - \'h337\' is not defined. (no-undef)\n45:5 - Unexpected console statement. (no-console)');
+  });
+});
+define('dummy/initializers/container-debug-adapter', ['exports', 'ember-resolver/resolvers/classic/container-debug-adapter'], function (exports, _containerDebugAdapter) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    name: 'container-debug-adapter',
+
+    initialize: function initialize() {
+      var app = arguments[1] || arguments[0];
+
+      app.register('container-debug-adapter:main', _containerDebugAdapter.default);
+      app.inject('container-debug-adapter:main', 'namespace', 'application:main');
+    }
+  };
+});
+define('dummy/initializers/export-application-global', ['exports', 'ember', 'dummy/config/environment'], function (exports, _ember, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.initialize = initialize;
+  function initialize() {
+    var application = arguments[1] || arguments[0];
+    if (_environment.default.exportApplicationGlobal !== false) {
+      var theGlobal;
+      if (typeof window !== 'undefined') {
+        theGlobal = window;
+      } else if (typeof global !== 'undefined') {
+        theGlobal = global;
+      } else if (typeof self !== 'undefined') {
+        theGlobal = self;
+      } else {
+        // no reasonable global, just bail
+        return;
+      }
+
+      var value = _environment.default.exportApplicationGlobal;
+      var globalName;
+
+      if (typeof value === 'string') {
+        globalName = value;
+      } else {
+        globalName = _ember.default.String.classify(_environment.default.modulePrefix);
+      }
+
+      if (!theGlobal[globalName]) {
+        theGlobal[globalName] = application;
+
+        application.reopen({
+          willDestroy: function willDestroy() {
+            this._super.apply(this, arguments);
+            delete theGlobal[globalName];
+          }
+        });
+      }
+    }
+  }
+
+  exports.default = {
+    name: 'export-application-global',
+
+    initialize: initialize
+  };
+});
+define('dummy/resolver', ['exports', 'ember-resolver'], function (exports, _emberResolver) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = _emberResolver.default;
+});
+define('dummy/router', ['exports', 'ember', 'dummy/config/environment'], function (exports, _ember, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+
+  var Router = _ember.default.Router.extend({
+    location: _environment.default.locationType,
+    rootURL: _environment.default.rootURL
+  });
+
+  Router.map(function () {});
+
+  exports.default = Router;
+});
+define('dummy/services/ajax', ['exports', 'ember-ajax/services/ajax'], function (exports, _ajax) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  Object.defineProperty(exports, 'default', {
+    enumerable: true,
+    get: function () {
+      return _ajax.default;
+    }
+  });
+});
+define("dummy/templates/application", ["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = Ember.HTMLBars.template({ "id": "M3FA8hcR", "block": "{\"statements\":[[1,[33,[\"ember-heatmap\"],null,[[\"config\",\"data\",\"max\"],[[28,[\"heatmapconfig\"]],[28,[\"heatmapdata\"]],[28,[\"heatmapmax\"]]]]],false],[0,\"\\n\"],[1,[26,[\"welcome-page\"]],false],[0,\"\\n\"],[0,\"\\n\"],[1,[26,[\"outlet\"]],false],[0,\"\\n\"]],\"locals\":[],\"named\":[],\"yields\":[],\"hasPartials\":false}", "meta": { "moduleName": "dummy/templates/application.hbs" } });
+});
+
+
+define('dummy/config/environment', ['ember'], function(Ember) {
+  var prefix = 'dummy';
+try {
+  var metaName = prefix + '/config/environment';
+  var rawConfig = document.querySelector('meta[name="' + metaName + '"]').getAttribute('content');
+  var config = JSON.parse(unescape(rawConfig));
+
+  var exports = { 'default': config };
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+  return exports;
+}
+catch(err) {
+  throw new Error('Could not read config from meta tag with name "' + metaName + '".');
+}
+
+});
+
+if (!runningTests) {
+  require("dummy/app")["default"].create({});
+}
+//# sourceMappingURL=dummy.map
